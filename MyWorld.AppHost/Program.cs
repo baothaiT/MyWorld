df737363b4b3bb4878567d088b4724563f287dec
BAO\thaib <thaibao0225@gmail.com>
2025-08-05 02:12:06 +0700

    update code

--- MyWorld.AppHost/Program.cs
+++ MyWorld.AppHost/Program.cs
@@ -19,7 +19,7 @@ var sql = builder.AddSqlServer(
 
 var MyWorldDb = sql.AddDatabase("MyWorldDb");
 
-builder.AddProject<Projects.MyWorld_API>(nameof(ServiceNameEnum.MyWorldAPI))
+var myWorldAPI = builder.AddProject<Projects.MyWorld_API>(nameof(ServiceNameEnum.MyWorldAPI))
     .WaitFor(MyWorldDb)
     .WithReference(MyWorldDb)
     ;
@@ -28,5 +28,13 @@ builder.AddProject<Projects.MyWorld_HealthUI>(nameof(ServiceNameEnum.MyWorldHeal
     .WaitFor(MyWorldDb)
     .WithReference(MyWorldDb)
     ;
+
+// React UI
+// Provide the correct path to the npm executable or use a valid overload
+builder.AddExecutable("react-ui", "npm", workingDirectory: "../MyWorld.AdminUI/myworld-dashboard")
+    .WithArgs("start")
+    .WithHttpEndpoint(port: 3001, name: "http") // React dev server default
+    .WaitFor(myWorldAPI)
+    .WithReference(myWorldAPI);
 
 builder.Build().Run();
